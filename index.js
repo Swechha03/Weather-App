78f1803102ebc5e9321c40e88c7414ce28c9f19f
Swechha03 <Swechhashrestha8@gmail.com>
2025-06-21 16:05:08 -0700

    Fixed units of the measurement

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { apiKey } from './.gitignore/config.js'
+import { apiKey } from './config.js'
 
 document.querySelector('.search-button').addEventListener('click', () => {
     let city = document.querySelector('.search-city').value;
@@ -6,10 +6,10 @@ document.querySelector('.search-button').addEventListener('click', () => {
 });
 
 async function getWeather(city) {
-    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
     const response = await fetch(apiURL);
     const data = await response.json();
 
-    document.querySelector('.temperature').innerHTML = data.main.temp;
-    document.querySelector('.humidity').innerHTML = data.main.humidity;
+    document.querySelector('.temperature').innerHTML = `${Math.round(data.main.temp)}°F`;
+    document.querySelector('.humidity').innerHTML = `${Math.round(data.main.humidity)}%`;
 }
